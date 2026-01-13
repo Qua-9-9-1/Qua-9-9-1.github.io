cb1199a76bc169c7370a141ac6f61a6c993336e6
Quentin D <quaduval@gmail.com>
2026-01-13 22:58:14 +0100

    [FIX] reset config to default

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/Qua-9-9-1.github.io/";
-  }
-  return config;
-});
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+})
