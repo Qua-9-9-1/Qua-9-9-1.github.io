3b84e4ed73fc9db92c7d8a0880d75d8235673c43
Quentin D <quaduval@gmail.com>
2026-01-15 18:02:38 +0100

    [EDIT] move index.css to styles folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './style.css';
+import "./styles/index.css";
 import { LanguageProvider } from './context/LanguageContext';
 import { ThemeProvider } from './context/ThemeContext';
 
