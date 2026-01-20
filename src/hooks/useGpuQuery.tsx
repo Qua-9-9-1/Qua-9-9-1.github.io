b6cf219f1864e67efce22324708bb8e76e8ffd04
Quentin D <quaduval@gmail.com>
2026-01-20 17:51:29 +0100

    [EDIT] code format

--- src/hooks/useGpuQuery.tsx
+++ src/hooks/useGpuQuery.tsx
@@ -9,19 +9,22 @@ export function useGpuTier() {
 
   useEffect(() => {
     try {
-    const canvas = document.createElement('canvas');
-    const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+      const canvas = document.createElement('canvas');
+      const gl =
+        canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
 
-    if (gl && gl instanceof WebGLRenderingContext) {
+      if (gl && gl instanceof WebGLRenderingContext) {
         const debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
 
         if (debugInfo) {
           const renderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
           const vendor = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
-            const isLowEnd = /(mali|adreno|powervr|intel)/i.test(renderer + vendor);
-            setGpuInfo({ renderer, vendor, isLowEnd });
-          }
+          const isLowEnd = /(mali|adreno|powervr|intel)/i.test(
+            renderer + vendor
+          );
+          setGpuInfo({ renderer, vendor, isLowEnd });
         }
+      }
     } catch {
       setGpuInfo(null);
     }
